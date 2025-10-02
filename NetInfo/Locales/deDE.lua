353cb59f0ac0d25cc7bcd8a10e1f1f99144a36bb
William <60353638+coldbootx@users.noreply.github.com>
2025-10-02 19:53:54 +0000

    Add files via upload

---
+++ NetInfo/Locales/deDE.lua
@@ -0,0 +1,16 @@
+local _, NetInfoAddon = ...
+NetInfoAddon.L = NetInfoAddon.L or {}
+local L = NetInfoAddon.L
+
+L["TITLE"] = "NetInfo Steuerung"
+L["SHIFT_LEFT"] = "Shift + Linksklick:"
+L["MOVE"] = "Bewegen"
+L["SHIFT_RIGHT"] = "Shift + Rechtsklick:"
+L["HIDE_SHOW"] = "Ein-/Ausblenden"
+L["RESIZE"] = "Größe ändern:"
+L["DRAG_CORNER"] = "Ziehe rechte untere Ecke"
+L["COMMAND_HELP"] = "Zeige Hilfe"
+L["COMMAND_VERSION"] = "Addon Version anzeigen"
+L["COMMAND_SHOW"] = "Frame anzeigen"
+L["COMMAND_HIDE"] = "Frame ausblenden"
+L["UNKNOWN_COMMAND"] = "Unbekannter Befehl. Tippe /netinfo help"
